5e323a8f1bd612fea96bb87989e5b76b00e26d27
dajamplay <ggyoug@gmail.com>
2023-10-06 12:27:56 +0300

    refactor: change routes structure

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,15 @@
 <?php
 
-use App\Http\Controllers\Web\Auth;
 use App\Http\Controllers\Web\Main;
 use Illuminate\Support\Facades\Route;
 
+require_once './web/auth.php';
+
 Route::middleware('auth')->group( function () {
-
-    Route::post('/logout', [Auth\LoginController::class, 'destroy'])
-        ->name('logout');
-
-    Route::get('/email/verify/{id}/{hash}', Auth\VerifyEmailController::class)
-        ->middleware('signed')
-        ->name('verification.verify');
-
-    Route::get('/email/verify', Auth\EmailVerificationController::class)
-        ->name('verification.notice');
-
-    Route::post('/email/verification-notification', Auth\EmailVerificationNotificationController::class)
-        ->name('verification.send');
 
     Route::get('/dashboard', function () {
         return 'Is auth user... Dashboard...';
     })->middleware('verified');
-
-});
-
-Route::middleware('guest')->group( function () {
-
-    Route::get('/login', [Auth\LoginController::class, 'create'])
-        ->name('login');
-    Route::post('/login', [Auth\LoginController::class, 'store']);
-
-    Route::get('/register', [Auth\RegisterController::class, 'create'])
-        ->name('register');
-    Route::post('/register', [Auth\RegisterController::class, 'store']);
-
-    Route::get('/forgot-password', [Auth\ForgotPasswordController::class, 'create'])
-        ->name('password.request');
-    Route::post('/forgot-password', [Auth\ForgotPasswordController::class, 'store'])
-        ->name('password.email');
-
-    Route::get('/reset-password', [Auth\ResetPasswordController::class, 'create'])
-        ->name('password.reset');
-    Route::post('/reset-password', [Auth\ResetPasswordController::class, 'store'])
-        ->name('password.update');
 
 });
 
