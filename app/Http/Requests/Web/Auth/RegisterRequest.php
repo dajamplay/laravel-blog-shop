610361aef784d2e48067359606cb041b05682b84
Maksim Aksenov <ggyoug@gmail.com>
2023-10-07 11:49:21 +0300

    refactor(request):
    delete comments

--- app/Http/Requests/Web/Auth/RegisterRequest.php
+++ app/Http/Requests/Web/Auth/RegisterRequest.php
@@ -18,7 +18,7 @@ class RegisterRequest extends FormRequest
             'last_name' => ['required', 'string', 'max:50'],
             'email' => ['required', 'string', 'max:100', 'email', 'unique:users,email'],
             'password' => ['required', 'string', 'min:3', 'confirmed'],
-            "birthday" => ['nullable', 'string', 'date'] // 2021-10-09/09-10-2021 12:30:00
+            "birthday" => ['nullable', 'string', 'date']
         ];
     }
 }
