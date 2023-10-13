dda609ce1f9186f66c43d5407e1848b06e0247a8
dajamplay <ggyoug@gmail.com>
2023-10-13 21:38:54 +0300

    feat: translate
    add custom translate auth

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -8,7 +8,7 @@
         <div class="login-logo"><b>{{ __('Подтверждение почты') }}</b></div>
         <div class="card">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">{{ __('Подтверждение почты') }}</p>
+                <p class="login-box-msg">{{ __('Если Вы не получили ссылку для подтверждения электронной почты, Вы можете запросить ссылку повторно.') }}</p>
                 @if(session('status'))
                     <div class="alert alert-success alert-dismissible">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
@@ -19,7 +19,7 @@
                     @csrf
                     <div class="col-12">
                         <button type="submit"
-                                class="btn btn-primary btn-block">{{ __('отправить подтверждение почты') }}</button>
+                                class="btn btn-primary btn-block">{{ __('Отправить ссылку') }}</button>
                     </div>
                 </form>
             </div>
